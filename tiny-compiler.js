69d93a2057136c2468647dae75028460350b8b0e
bertdev <herbertmatos10@hotmail.com>
2023-11-20 15:28:25 -0300

    add tranverser and transformer for our compiler

--- tiny-compiler.js
+++ tiny-compiler.js
@@ -141,4 +141,85 @@ function parser(tokens) {
   return ast;
 }
 
+function transverser(ast, visitor) {
+  function transverseArray(array, parent) {
+    array.foreach(child => transverseNode(child, parent));
+  }
+
+  function transverseNode(node, parent) {
+    let method = visitor[node.type];
+    if (method && method.enter) {
+      method.enter(node,parent);
+    }
+
+    switch (node.type) {
+      case "Program":
+        transverseArray(node.body, node);
+        break;
+      case "CallExpression":
+        transverseArray(node.params, node);
+        break;
+      case "NumberLiteral":
+      case "StringLiteral":
+        break;
+      default:
+        throw new TypeError(node.type);
+    }
+
+    if (method && method.exit) {
+      method.exit(node, parent);
+    }
+  }
+
+  transverseNode(ast, null);
+}
+
+function transformer(ast) {
+  let newAst = {
+    type: "Program",
+    body: [],
+  };
+
+  ast._context = newAst.body;
+
+  transverser(ast,{
+    NumberLiteral: {
+      enter(node, parent) {
+        parent._context.push({type: "NumberLiteral", value: node.value});
+      }
+    },
+    StringLiteral: {
+      enter(node, parent) {
+        parent.context.push({type: "StringLiteral", value: node.value});
+      }
+    },
+    CallExpression: {
+      enter(node, parent) {
+        let expression = {
+          type: "CallExpression",
+          calle: {
+            type: "Identifier",
+            name: node.name
+          },
+          arguments: []
+        };
+
+        node._context = expression.arguments;
+
+        if (parent.type !== "CallExpression") {
+          expression = {
+            type: "ExpressionStatement",
+            expression: expression
+          };
+        }
+
+        parent._context.push(expression);
+      }
+    }
+  });
+
+  return newAst;
+}
+
+
 
